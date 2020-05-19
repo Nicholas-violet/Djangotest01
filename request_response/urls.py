25f46b4801774c89848fe91f53a43dd05d1f596e
温海辉 <Nicholas_violet@163.com>
2020-05-20 02:02:42 +0800

    获取表单类型请求体数据，----POST.get()

--- request_response/urls.py
+++ request_response/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     # 测试提取查询字符串参数：http://127.0.0.1:8000/querystring/?name=zxc&age=18
     path('querystring/', views.QSParamView.as_view()),
+    path('formdata/', views.FormDataParamView.as_view()),
 ]
 
--- request_response/views.py
+++ request_response/views.py
@@ -4,7 +4,7 @@ from django.views import View
 # Create your views here.
 class QSParamView(View):
     """测试提取查询参数字符串
-
+    http://127.0.0.1:8000/querystring/?name=温海辉&age=120
     """
 
     def get(self,request):
@@ -13,3 +13,14 @@ class QSParamView(View):
         age = request.GET.get('age','18')
 
         return http.HttpResponse('查询字符串参数，%s--%s'%(name,age))
+
+class FormDataParamView(View):
+    """用于提取表单类型请求体参数
+    http://127.0.0.1:8000/formdata/
+    """
+
+    def post(self,request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        return http.HttpResponse('表单类型请求体参数,%s--%s'%(username,password))
